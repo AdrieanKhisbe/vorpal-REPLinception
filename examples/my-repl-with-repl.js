4845f3bfd0fd57c85e4c460fb1879a95dd0221d8
Adriean Khisbe <adriean.khisbe@live.fr>
2018-01-05 16:44:18 +0100

    init: add examples folder

---
+++ examples/my-repl-with-repl.js
@@ -0,0 +1,9 @@
+const Vorpal = require('vorpal');
+const vorpalReplinception = require('../lib');
+
+const vorpal = Vorpal();
+
+vorpal
+  .delimiter('node~$')
+  .use(vorpalReplinception)
+  .show();
