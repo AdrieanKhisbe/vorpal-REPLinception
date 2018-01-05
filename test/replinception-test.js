bcf7b9532cd4b2e10046588154bc901cebfc55c5
Adriean Khisbe <adriean.khisbe@live.fr>
2018-01-05 17:50:53 +0100

    test: init tests structure

---
+++ test/replinception-test.js
@@ -0,0 +1,15 @@
+import {describe} from 'ava-spec';
+import vorpalReplinception from '../lib'
+
+describe('replinception with no config', it => {
+
+
+
+});
+
+
+describe('replinception with some config', it => {
+
+
+
+});
