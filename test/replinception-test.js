a695c02bd50dd82fa10db9de3fe3be777765faec
Adriean Khisbe <adriean.khisbe@live.fr>
2018-01-19 14:37:57 +0100

    test: write two first tests

--- test/replinception-test.js
+++ test/replinception-test.js
@@ -1,10 +1,21 @@
 import {describe} from 'ava-spec';
-import vorpalReplinception from '../lib'
+import Vorpal from 'vorpal';
+import replinception from '../lib'
 
 describe('replinception with no config', it => {
-
-
-
+  it('defines the correct command', t => {
+    const vorpal = Vorpal();
+    vorpal.use(replinception());
+    const cmd = vorpal.find('repl')
+    t.truthy(cmd);
+  });
+  it('command has correct details', t => {
+    const vorpal = Vorpal();
+    vorpal.use(replinception());
+    const cmd = vorpal.find('repl')
+    t.deepEqual(cmd._description, 'Open a node REPL. (alias console)')
+    t.deepEqual(cmd._aliases, ['console']);
+  });
 });
 
 
