0e9aeb2207edf2c007773d283634954680ac0a53
AdrieanKhisbe <adriean.khisbe@live.fr>
2020-03-29 20:33:22 +0200

    doc: improve cli examples with instructions :closed_book:

--- examples/my-repl-with-context-repl.js
+++ examples/my-repl-with-context-repl.js
@@ -5,11 +5,14 @@ const vorpalReplinception = require('../src');
 
 const vorpal = Vorpal();
 
+console.log(chalk.blue(`Type ${chalk.bold('repl')} to open a node repl in this vorpal app`));
 vorpal
   .delimiter('node~$')
   .use(
     vorpalReplinception({
-      enterMessage: 'In the context there is some chalk, a universe and myLibrary',
+      enterMessage: `In the context there is some ${chalk.dim('chalk')}, a ${chalk.dim(
+        'universe'
+      )} and ${chalk.dim('myLibrary')}`,
       // The following context will be accessible from within the node repl
       context: {
         chalk,
--- examples/my-repl-with-repl.js
+++ examples/my-repl-with-repl.js
@@ -3,6 +3,7 @@ const vorpalReplinception = require('../src');
 
 const vorpal = Vorpal();
 
+console.log("Type 'repl' to open a node repl in this vorpal app");
 vorpal
   .delimiter('node~$')
   .use(vorpalReplinception())
